aff3e0a49333a13126137ec0487b5a4dbdb28572
Rodrigo Fuentes <rodrifnts@gmail.com>
2018-09-01 17:01:49 -0400

    prueba de git clone

--- Perfiles/resources/views/welcome.blade.php
+++ Perfiles/resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    PERFILES
                 </div>
 
                 <div class="links">
