39e8522e15076ab80f1e1d823a5051bd71d6c555
marcos <marcoschile9999@gmail.com>
2018-09-19 12:11:06 +0100

    puer

--- Perfiles/resources/views/welcome.blade.php
+++ Perfiles/resources/views/welcome.blade.php
@@ -1,4 +1,4 @@
 @extends('plantilla')
 @section('contenido')
-    <h1 class="display-3">Sistema de Registro de Perfiles de Proyectos Finales de Grado </h1>
+    <h1 class="display-3">Sistema de Registro de Perfiles de Proyectos Finales de Grado23 </h1>
 @endsection
