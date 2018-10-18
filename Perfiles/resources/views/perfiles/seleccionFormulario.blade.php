b2f8fdb096955ea0cb753cc091a97da15725925c
Rodrigo Fuentes <rodrifnts@gmail.com>
2018-10-18 17:00:32 -0400

    formulario de tesis y proyGrado incompleto

--- Perfiles/resources/views/perfiles/seleccionFormulario.blade.php
+++ Perfiles/resources/views/perfiles/seleccionFormulario.blade.php
@@ -20,15 +20,28 @@
                     <select class="form-control" id="modalidad" name="modalidad">
                         <option>seleccione una opcion</option>
                         @foreach($modalidades as $modalidad)
-                            <option>{{$modalidad->nombre_mod}}</option>
+                            <option>{{$modalidad}}</option>
                         @endforeach
                     </select>
                 </div>
             </div>
+
+            <div class="form-group row">
+                <label for="area" class="col-sm-3 col-form-label">Area de su perfil</label>
+                <div class="col-sm-6">
+                    <select class="form-control" id="area" name="area">
+                        <option>seleccione una opcion</option>
+                        @foreach($areas as $area)
+                            <option value="{{$area->id}}">{{$area->nombre}}</option>
+                        @endforeach
+                    </select>
+                </div>
+            </div>
+
             <div class="form-group row">
                 <div class="col-sm-7 offset-2 mt-4">
                     <a href="{{route('menu')}}" class="btn btn-danger mr-2">retornar menu</a>
-                    <button type="submit"  class='btn btn-success registrar'>Seleccionar</button>
+                    <button type="submit"  class="btn btn-success ">Seleccionar</button>
                 </div>
             </div>
         </form>
