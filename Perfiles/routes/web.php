24147bc68086ebe23fc9a942cacfc6dabd97a78e
Raul Alejandro <dejota423@gmail.com>
2018-12-11 18:32:17 -0400

    corregido error de los menus

--- Perfiles/routes/web.php
+++ Perfiles/routes/web.php
@@ -25,12 +25,12 @@ Route::get('/prueba', function () {
 
 //Roles
 Route::get('/menu','menuController@index')->name('menu');
-Route::get('/roles','RoleController@index')->name('roles');
-Route::get('/roles/crear','RoleController@crear')->name('crearRol');
-Route::post('/roles/guardar','RoleController@guardar')->name('guardarRol');
-Route::get('/roles/{role}/editar','RoleController@editar')->name('editarRol');
-Route::any('/roles/{role}','RoleController@actualizar')->name('actualizarRol');
-Route::any('/roles/{role}/eliminar','RoleController@eliminar')->name('eliminarRol');
+Route::get('roles','RoleController@index')->name('roles');
+Route::get('roles/crear','RoleController@crear')->name('crearRol');
+Route::post('roles/guardar','RoleController@guardar')->name('guardarRol');
+Route::get('roles/{role}/editar','RoleController@editar')->name('editarRol');
+Route::any('roles/{role}','RoleController@actualizar')->name('actualizarRol');
+Route::any('roles/{role}/eliminar','RoleController@eliminar')->name('eliminarRol');
 
 //usuarios
 Route::get('/usuarios','usuarioController@index')->name('usuarios');
@@ -92,30 +92,38 @@ Route::post('/modalidad/importacion','modalidades@importacion')->name('importaci
 
 
 //carreras
-Route::get('/carreras','CarreraController@index')->name('carreras');
-Route::get('/carreras/crear','CarreraController@crear')->name('crearCarrera');
-Route::post('/carreras/guardar','CarreraController@guardar')->name('guardarCarrera');
+Route::get('inicio/carreras','CarreraController@index')->name('carreras');
+Route::get('inicio/carreras/crear','CarreraController@crear')->name('crearCarrera');
+Route::post('inicio/carreras/guardar','CarreraController@guardar')->name('guardarCarrera');
 //Route::get('/carreras/{carrera}','CarreraController@detalle')->name('detalleCarrera');
-Route::get('/carreras/{carrera}/editar','CarreraController@editar')->name('editarCarrera');
-Route::put('/carreras/{carrera}','CarreraController@actualizar')->name('actualizarCarrera');//put metodo para actualizar
-Route::delete('/carreras/{carrera}/eliminar','CarreraController@eliminar')->name('eliminarCarrera');
-Route::get('/carreras/importar','CarreraController@importar')->name('importarCarreras');
-Route::post('/carreras/importacion','CarreraController@importacion')->name('importacionCarrera');
-Route::any('/carreras/agregarArea/{carrera}', 'CarreraController@areas')->name('areasCarrera');
-Route::any('/carreras/agregarArea/almacenar/{carrera}', 'CarreraController@almacenarArea')->name('almacenarAreasCarrera');
-Route::any('/carreras/eliminarArea/{carrera}/{area}', 'CarreraController@EliminarArea')->name('eliminarCarreraArea');
+Route::get('inicio/carreras/{carrera}/editar','CarreraController@editar')->name('editarCarrera');
+Route::put('inicio/carreras/{carrera}','CarreraController@actualizar')->name('actualizarCarrera');//put metodo para actualizar
+Route::delete('inicio/carreras/{carrera}/eliminar','CarreraController@eliminar')->name('eliminarCarrera');
+Route::get('inicio/carreras/importar','CarreraController@importar')->name('importarCarreras');
+Route::post('inicio/carreras/importacion','CarreraController@importacion')->name('importacionCarrera');
+Route::any('inicio/carreras/agregarArea/{carrera}', 'CarreraController@areas')->name('areasCarrera');
+Route::any('inicio/carreras/agregarArea/almacenar/{carrera}', 'CarreraController@almacenarArea')->name('almacenarAreasCarrera');
+Route::any('inicio/carreras/eliminarArea/{carrera}/{area}', 'CarreraController@EliminarArea')->name('eliminarCarreraArea');
+
+//Gestion
+Route::any('inicio/gestion', 'GestionController@index')->name('gestiones');
+Route::get('inicio/gestion/registrar', 'GestionController@registrar')->name('registrarGestion');
+Route::any('inicio/gestion/registrar/almacenar', 'GestionController@almacenar')->name('almacenarGestion');
+Route::get('inicio/gestion/editar/{gestion}', 'GestionController@editar')->name('editarGestion');
+Route::post('inicio/gestion/editar/modificar/{gestion}', 'GestionController@modificar')->name('modificarGestion');
+
 //docentes
-Route::get('/docentes','docenteController@index')->name('Docentes');
-Route::get('/docentes/registrar', 'docenteController@registrar')->name('registrarDocente')->middleware('permisos:docentes');
-Route::any('/docentes/registrar/almacenar','docenteController@almacenar')->name('almacenarDocente')->middleware('permisos:docentes');
-Route::any('/docentes/editar/{docente}','docenteController@editar')->name('editarDocente');
-Route::any('/docentes/editar/modificar/{docente}','docenteController@modificar')->name('modificarDocente');
-Route::any('/docentes/eliminar/{docente}','docenteController@eliminar')->name('eliminarDocente');
-Route::any('/docentes/ver/{docente}','docenteController@ver')->name('verDocente');
-Route::get('/cambiar_contraseña_docente/{docente}','docenteController@cambiarContraseña')->name('cambiarContraseñaDocente');
-Route::post('/guardar_contraseña_docente/{docente}','docenteController@guardarContraseña')->name('guardarContraseñaDocente');
-Route::get('/tutoria/{docente}','docenteController@tutoria')->name('tutoriaDocente');
-Route::get('/exportar_pdf_tutoria/{docente}','docenteController@descargarPdf')->name('tutoriaDocentePdf');
+Route::get('docentes','docenteController@index')->name('Docentes');
+Route::get('docentes/registrar', 'docenteController@registrar')->name('registrarDocente')->middleware('permisos:docentes');
+Route::any('docentes/registrar/almacenar','docenteController@almacenar')->name('almacenarDocente')->middleware('permisos:docentes');
+Route::any('docentes/editar/{docente}','docenteController@editar')->name('editarDocente');
+Route::any('docentes/editar/modificar/{docente}','docenteController@modificar')->name('modificarDocente');
+Route::any('docentes/eliminar/{docente}','docenteController@eliminar')->name('eliminarDocente');
+Route::any('docentes/ver/{docente}','docenteController@ver')->name('verDocente');
+Route::get('docentes/cambiar_contraseña_docente/{docente}','docenteController@cambiarContraseña')->name('cambiarContraseñaDocente');
+Route::post('docentes/guardar_contraseña_docente/{docente}','docenteController@guardarContraseña')->name('guardarContraseñaDocente');
+Route::get('docentes/tutoria/{docente}','docenteController@tutoria')->name('tutoriaDocente');
+Route::get('docentes/exportar_pdf_tutoria/{docente}','docenteController@descargarPdf')->name('tutoriaDocentePdf');
 
 //estudiantes
 Route::get('/estudiantes','EstudianteController@index')->name('estudiantes');
@@ -161,12 +169,6 @@ Route::any('/perfil/publicar/{perfil}','PerfilController@publicar')->name('publi
 Route::any('/perfil/eliminar/{perfil}','PerfilController@eliminar')->name('eliminarPerfil')->middleware('permisos:eliminar_perfil');
 Route::any('/perfil/cambiarEstado/{perfil}','PerfilController@cambiarEstado')->name('cambiarEstadoPerfil')->middleware('permisos:cambiar_estado_perfil');
 
-//Gestion
-Route::any('/menu/Gestion', 'GestionController@index')->name('gestiones');
-Route::get('menu/Gestion/registrar', 'GestionController@registrar')->name('registrarGestion');
-Route::any('menu/Gestion/registrar/almacenar', 'GestionController@almacenar')->name('almacenarGestion');
-Route::get('menu/Gestion/editar/{gestion}', 'GestionController@editar')->name('editarGestion');
-Route::post('menu/Gestion/editar/modificar/{gestion}', 'GestionController@modificar')->name('modificarGestion');
 
 // E-mail verification
 Route::get('/register/verify/{code}', 'Auth\RegisterController@verify');
@@ -176,8 +178,8 @@ Route::get('/register/verify/{code}', 'Auth\RegisterController@verify');
 Route::get('/notificar','menuController@notificar')->name('notificar');
 
 //reportes
-Route::get('/generar_reportes','ReportesController@generar')->name('generar')->middleware('permisos:reportes');
-Route::post('/generador/reportes','ReportesController@generador')->name('generador')->middleware('permisos:reportes');
-Route::get('/reportes/pdf','ReportesController@reporte')->name('reportePdf')->middleware('permisos:reportes');
+Route::get('reportes/generar_reportes','ReportesController@generar')->name('generar')->middleware('permisos:reportes');
+Route::post('reportes/generador/reportes','ReportesController@generador')->name('generador')->middleware('permisos:reportes');
+Route::get('reportes/pdf','ReportesController@reporte')->name('reportePdf')->middleware('permisos:reportes');
 
-Route::get('/guiaFormulario','guiaController@index')->name('guiaFormulario');
+Route::get('inicio/guiaFormulario','guiaController@index')->name('guiaFormulario');
